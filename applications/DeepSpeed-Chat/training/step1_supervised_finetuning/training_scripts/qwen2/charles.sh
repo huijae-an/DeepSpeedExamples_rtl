b06697b5d3e800e8fb21a656fc21ca522402287f
Charles Hong <charleshong@berkeley.edu>
2025-05-26 00:16:55 -0700

    changes

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/qwen2/charles.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/qwen2/charles.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-OUTPUT=./qwen2.5-32b-coder-instruct-verilog_completion_gpt_4o_augmented
-GARBAGE=/scratch/huijaean/garbage/*
+OUTPUT=./qwen2.5-7b-coder-instruct-pymtl_gpt_4o_augmented
+GARBAGE=/scratch/charleshong/garbage/*
 PREVIOUS_MODEL_CACHE=$HOME/.cache/huggingface/hub/*
 ZERO_STAGE=3
 mkdir -p $OUTPUT
-rm -rf $GARBAGE
+# rm -rf $GARBAGE
 # rm -rf $PREVIOUS_MODEL_CACHE
 
 
 deepspeed --master_port 50000 main.py \
    --data_path local/jsonfile \
    --data_split "10,0,0" \
-   --data_output_path /scratch/huijaean/garbage \
-   --model_name_or_path Qwen/Qwen2.5-Coder-32B-Instruct \
-   --per_device_train_batch_size 4 \
-   --per_device_eval_batch_size 4 \
+   --data_output_path /scratch/charleshong/garbage \
+   --model_name_or_path Qwen/Qwen2.5-Coder-7B-Instruct \
+   --per_device_train_batch_size 6 \
+   --per_device_eval_batch_size 6 \
    --max_seq_len 1024 \
    --learning_rate 1e-5 \
    --num_train_epochs 1  \
-   --gradient_accumulation_steps 16 \
+   --gradient_accumulation_steps 32 \
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
    --seed 1234 \
