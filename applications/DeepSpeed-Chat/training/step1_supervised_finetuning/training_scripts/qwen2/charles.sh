1be0729d4ca9c0ee544d54801a92f61233f47b1e
Huijae An <huijae.an@firesim2.Millennium.Berkeley.EDU>
2025-05-25 17:11:37 -0700

    latest deepspeed

---
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/qwen2/charles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+OUTPUT=./qwen2.5-32b-coder-instruct-verilog_completion_gpt_4o_augmented
+GARBAGE=/scratch/huijaean/garbage/*
+PREVIOUS_MODEL_CACHE=$HOME/.cache/huggingface/hub/*
+ZERO_STAGE=3
+mkdir -p $OUTPUT
+rm -rf $GARBAGE
+# rm -rf $PREVIOUS_MODEL_CACHE
+
+
+deepspeed --master_port 50000 main.py \
+   --data_path local/jsonfile \
+   --data_split "10,0,0" \
+   --data_output_path /scratch/huijaean/garbage \
+   --model_name_or_path Qwen/Qwen2.5-Coder-32B-Instruct \
+   --per_device_train_batch_size 4 \
+   --per_device_eval_batch_size 4 \
+   --max_seq_len 1024 \
+   --learning_rate 1e-5 \
+   --num_train_epochs 1  \
+   --gradient_accumulation_steps 16 \
+   --lr_scheduler_type cosine \
+   --num_warmup_steps 0 \
+   --seed 1234 \
+   --gradient_checkpointing \
+   --dtype bf16 \
+   --zero_stage $ZERO_STAGE \
+   --deepspeed \
+   --lora_dim 32 \
+   --lora_module_name "layers." \
+   --output_dir $OUTPUT \
+   | tee $OUTPUT/training.log
